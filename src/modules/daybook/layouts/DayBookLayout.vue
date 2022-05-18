bd2fff804526a362fc98a526ff1a94915d6ff07e
JaviMx <javimxmine@hotmail.com>
2022-05-18 17:44:47 -0500

    Funcion IsLoading agregada

--- src/modules/daybook/layouts/DayBookLayout.vue
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -1,6 +1,17 @@
 <template>
     <NavbarDayBook/>
-    <div class="d-flex">
+
+    <div v-if="isLoading" class="row justify-content-md-center">
+        <div class="col-3 alert-info text-center mt-5">
+            Espere por favor...
+            <div class="mt-2">
+                <i class="fa fa-spin fa-sync"></i>
+            </div>
+        </div>
+
+    </div>
+
+    <div v-else class="d-flex">
         <div class="col-4">
             <EntryList />
         </div>
@@ -15,7 +26,7 @@
 <script>
 
 import {defineAsyncComponent} from "vue";
-import {mapActions} from 'vuex'
+import {mapActions, mapState} from 'vuex'
 
 export default {
     components: {
@@ -23,7 +34,11 @@ export default {
         EntryList: defineAsyncComponent( () => import('../components/EntryList.vue'))
     },
     methods: {
-        ...mapActions('journal', ['loadEntries'])
+        ...mapActions('journal', ['loadEntries']),
+
+    },
+    computed:{
+        ...mapState('journal', ['isLoading'])
     },
     created() {
         this.loadEntries()
